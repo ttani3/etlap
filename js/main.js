964d9511fdb73e245bdfca5c7f1170173a10cab2
Joe <t.aniko@outlook.com>
2021-08-12 23:24:50 +0200

    New files added

---
+++ js/main.js
@@ -0,0 +1,22 @@
+function calcAmount() {
+    let price = 1000;
+    let amountInput = document.querySelector("input[name='amount-input']");
+    let amountNumber = parseInt(amountInput.value);
+    /*amountNumber = isNaN(amountNumber) ? 0 : amountNumber;*/
+    showSumPrice(price, amountNumber)
+}
+
+function showSumPrice(price, amountNumber) {
+    let showAmount = document.querySelector("span.show-amount");
+    if (amountNumber > 10) {
+        alert("Maximum 10 terméket vásárolhat!");
+        return;
+    } else if (amountNumber < 1) {
+        alert("Minimum 1 terméket kell vásárolnia!");
+    } else if (isNaN(amountNumber)) {
+        alert("Nem helyes érték!");
+    } else {
+        let amount = amountNumber * price;
+        showAmount.innerHTML = amount;
+    }
+}
